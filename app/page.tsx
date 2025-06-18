49971b1b67ebfa77bf75caa733f865051ec5f866
Yufa <112290188+01001101CK@users.noreply.github.com>
2025-06-18 11:59:02 -0700

    add header

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import Search from "@/components/search";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Search />
     </div>
   );
 }
