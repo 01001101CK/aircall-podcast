625cf44ff9887767c93e4ac44f2b0541bbe2399f
Yufa <112290188+01001101CK@users.noreply.github.com>
2025-06-18 17:44:08 -0700

    hero section

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import Search from "@/components/search";
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Search />
+      <Hero />
     </div>
   );
 }
---
+++ components/hero.tsx
@@ -0,0 +1,35 @@
+import styles from './hero.module.css';
+import Image from 'next/image';
+
+export default function Hero() {
+  return (
+    <section className={styles.hero}>
+      <div className={styles.heroContent}>
+        <h1 className={styles.title}>
+          Discover audio stories<br />
+          that entertain,<br />
+          inform, and inspire.
+        </h1>
+        <div className={styles.subscribeSection}>
+          <span className={styles.subscribeLabel}>Subscribe to our podcasts:</span>
+          <div className={styles.podcastLinks}>
+            <a href="#" className={styles.podcastLink} aria-label="Spotify">
+              <Image src="/spotify.svg" alt="Spotify" width={84} height={30} />
+            </a>
+            <a href="#" className={styles.podcastLink} aria-label="Deezer">
+                          <Image src="/deezer.svg" alt="Deezer" width={84} height={30} />
+            </a>
+            <a href="#" className={styles.podcastLink} aria-label="Apple Podcast">
+                          <Image src="/apple.svg" alt="Apple" width={130} height={30} />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className={styles.heroImages}>
+              <Image className={styles.imageLeft} src="/Female-Yellow.png" alt="Podcast 1" width={250} height={250}/>
+              <Image className={styles.imageCenter} src="/Female-Blue.png" alt="Podcast 2" width={354} height={354} />
+              <Image className={styles.imageRight} src="/Male-pink.png" alt="Podcast 3" width={299} height={299} />
+      </div>
+    </section>
+  );
+} 
